7d6399f483dd3ee27d9fcc01a7dac6b7b7843909
dotku <weijingjaylin@gmail.com>
2024-07-07 03:11:59 -0700

    use NEXT_PUBLIC_DOMAIN first domain

--- src/utils/getDomain.js
+++ src/utils/getDomain.js
@@ -1,9 +1,9 @@
 export function getDomain() {
-  if (process.env.VERCEL_URL) {
-    return `https://${process.env.VERCEL_URL}`;
-  }
   if (process.env.NEXT_PUBLIC_DOMAIN) {
     return process.env.NEXT_PUBLIC_DOMAIN;
+  }
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`;
   }
   // Fallback for local development
   return "http://localhost:3000";
