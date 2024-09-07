23f396703ce33e1a07b025bcb014128f7fa9376f
Weijing Jay Lin <weijingjaylin@gmail.com>
2024-09-06 23:53:11 -0700

    update mobile view of the content

--- src/app/business/page.jsx
+++ src/app/business/page.jsx
@@ -25,11 +25,11 @@ export default function BusinessPage() {
           className="rounded-none"
         />
         <CardFooter className="flex-col bg-white/90 border-white/20 overflow-hidden absolute h-full w-full justify-center md:h-auto md:bottom-1 md:w-[calc(100%_-_8px)] shadow-small md:ml-1 z-10">
-          <h2 className="text-3xl mb-3 w-full">
+          <h2 className="text-2xl md:text-3xl mb-3 w-full">
             Welcome to JYTech Business Center!
           </h2>
           <div className="text-gray-600 w-full">
-            <p className="text-xl">
+            <p className="text-base md:text-xl">
               We help global business grow, we connected with golbal 1,000+
               organization and partners, and successful handle over 100 cases
               per year.
@@ -37,7 +37,7 @@ export default function BusinessPage() {
           </div>
         </CardFooter>
       </Card>
-      <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 mt-3">
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-4 mt-3">
         {data.map(({ name, summary, url }, idx) => (
           <Card key={idx}>
             <CardHeader className="font-bold">{name}</CardHeader>
@@ -46,7 +46,7 @@ export default function BusinessPage() {
                 <Markdown>{summary}</Markdown>
               </ReactMarkdownWrapper>
             </CardBody>
-            <CardFooter>
+            <CardFooter className="justify-end">
               <Link
                 isExternal={!url}
                 showAnchorIcon={!url}
