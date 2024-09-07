b9cc965a81032e4b293b7f470cc469190cd4d977
Weijing Jay Lin <weijingjaylin@gmail.com>
2024-09-07 01:01:24 -0700

    update layout

--- src/app/business/eb-5/page.jsx
+++ src/app/business/eb-5/page.jsx
@@ -15,7 +15,7 @@ import styled from "styled-components";
 const StyledCard = styled(Card)`
   p {
     font-size: 1.1rem;
-    margin-bottom: 8px;
+    margin-bottom: 16px;
   }
 `;
 
@@ -46,27 +46,30 @@ export default function EB5() {
             permanent residency (a green card). Here&apos;s how it works:
           </p>
           <p>
-            Investment Requirement: Investors must make a significant financial
-            investment in a U.S. business. The standard minimum investment is
-            $1,050,000, but it can be reduced to $800,000 if the investment is
-            in a Targeted Employment Area (TEA), which includes rural areas or
-            areas with high unemployment.
+            <span className="font-semibold">Investment Requirement:</span>
+            Investors must make a significant financial investment in a U.S.
+            business. The standard minimum investment is $1,050,000, but it can
+            be reduced to $800,000 if the investment is in a Targeted Employment
+            Area (TEA), which includes rural areas or areas with high
+            unemployment.
           </p>
           <p>
-            Job Creation: The investment must create or preserve at least 10
-            full-time jobs for U.S. workers within two years of the
-            investor&apos;s admission to the U.S.
+            <span className="font-semibold">Job Creation:</span> The investment
+            must create or preserve at least 10 full-time jobs for U.S. workers
+            within two years of the investor&apos;s admission to the U.S.
           </p>
           <p>
-            Green Card: In return for their investment, the investor and their
-            immediate family (spouse and unmarried children under 21) can apply
-            for U.S. permanent residency.
+            <span className="font-semibold">Green Card:</span> In return for
+            their investment, the investor and their immediate family (spouse
+            and unmarried children under 21) can apply for U.S. permanent
+            residency.
           </p>
           <p>
-            Regional Centers: Investors often choose to invest through EB-5
-            Regional Centers, which are government-approved entities that pool
-            investments for larger projects. These centers help manage the job
-            creation and economic requirements of the EB-5 program.
+            <span className="font-semibold">Regional Centers:</span> Investors
+            often choose to invest through EB-5 Regional Centers, which are
+            government-approved entities that pool investments for larger
+            projects. These centers help manage the job creation and economic
+            requirements of the EB-5 program.
           </p>
           <p>
             The program is especially popular for foreign nationals looking for
---
+++ src/app/contact-us/page.tsx
@@ -0,0 +1,3 @@
+export default function ContactUs() {
+    return <><h2 className="text-2xl">Contact Us</h2></>
+}
